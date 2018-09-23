3ef66a74cd78ac6cb643ce3cd70f7bc9b2901635
widiamka <43339745+widiamka@users.noreply.github.com>
2018-09-23 10:20:01 +0700

    Add files via upload

---
+++ PBO3_10117082_Latihan8_Pecahan/src/pbo3_10117082_latihan8_pecahan/PBO3_10117082_Latihan8_Pecahan.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo3_10117082_latihan8_pecahan;
+
+/**
+ *
+ * @author Nama :WIDIAMEGA K
+ *          Kelas : IF3
+ *          NIM : 10117082
+ */
+public class PBO3_10117082_Latihan8_Pecahan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        float f;
+        double d;
+        f = 1234567890.123456789F;
+        d = 1_234_567_890.123456789;
+        System.out.println("Float : " +f);
+        System.out.println("Double : " +d);
+    
+    }
+    
+}
